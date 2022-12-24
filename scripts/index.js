0ddb46994dbdce53148c6172aafc7d33ae365ad6
eduardo cano <eduardo_cano_perez@live.com.mx>
2022-12-24 11:00:08 -0600

    se crea la logica para el menu del carrito

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
 const mobileMenuIcon = document.querySelector('.menu-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
+const mobileCarIcon = document.querySelector('.shopingcar-icon');
+const mobilecar = document.querySelector('.shopingcar-menu');
 
 mobileMenuIcon.addEventListener('click', toggleMobileMenu);
+mobileCarIcon.addEventListener('click', toggleMobileCar);
 
 function toggleMobileMenu() {
     // const isAsideClosed = aside.classlist.contains('inactive');
@@ -11,4 +14,9 @@ function toggleMobileMenu() {
     // }
 
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMobileCar() {
+    
+    mobilecar.classList.toggle('inactive')
 }
