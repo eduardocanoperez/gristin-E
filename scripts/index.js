655a5d1b4db5036c9a54100a630e56400f10bb15
eduardo cano <eduardo_cano_perez@live.com.mx>
2022-12-28 11:34:09 -0600

    se agregan las imagenes para la main section (rotos)

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,23 @@
 const mobileMenuIcon = document.querySelector('.menu-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileCarIcon = document.querySelector('.shopingcar-icon');
-const mobilecar = document.querySelector('.shopingcar-menu');
+const mobileCar = document.querySelector('.shopingcar-menu');
 
 mobileMenuIcon.addEventListener('click', toggleMobileMenu);
 mobileCarIcon.addEventListener('click', toggleMobileCar);
 
 function toggleMobileMenu() {
-    // const isAsideClosed = aside.classlist.contains('inactive');
-
-    // if (!isAsideClosed) {
-    //     aside.classlist.add('inactive');
-    // }
-
+    const isMobileCarClosed = mobileCar.classList.contains('inactive');
+    if(!isMobileCarClosed) {
+        mobileCar.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleMobileCar() {
-    
-    mobilecar.classList.toggle('inactive')
+function toggleMobileCar() { 
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    mobileCar.classList.toggle('inactive');
 }
